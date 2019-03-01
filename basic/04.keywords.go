9bfde84c74a0666b29d21a55e3e71dab71ad5edb
Nixawk <hap.ddup@gmail.com>
2019-03-01 22:27:44 +0800

    update keywords

--- basic/04.keywords.go
+++ basic/04.keywords.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
+        var (
+                a = 1
+                b = 2
+        )
 
+        fmt.Printf("a + b = %d\n", a+b)
 }
 
 /*
  * Keywords:
  *
- *     break      default      func      interface      select 
+ *     break      default      func      interface      select
  *     case       defer        Go        map            Struct
  *     chan       else         Goto      package        Switch
  *     const      fallthrough  if        range          Type
