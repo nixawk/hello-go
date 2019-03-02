b1794da6f20b08b986b245bad4fd1046cac7f285
nixawk <hap.ddup@gmail.com>
2019-03-02 15:26:21 +0800

    add channel

--- basic/26.channels.go
+++ basic/26.channels.go
@@ -1,16 +1,37 @@
 package main
 
 import "fmt"
+import "time"
+
+func send(c chan<- string) {
+	for {
+		c <- "ping"
+	}
+}
+
+func recv(c <-chan string) {
+	for {
+		msg := <-c
+		fmt.Printf("[recv]: %s\n", msg)
+		time.Sleep(time.Second * 1)
+	}
+}
 
 func main() {
-	messages := make(chan string)      /* create a new channel with make */
-	go func() { messages <- "ping" }() /* send a value into a channel */
+	var s string
 
-	msg := <-messages /* receive a value from the channel */
-	fmt.Println(msg)
+	c := make(chan string)
+	go send(c)
+	go recv(c)
+
+	fmt.Scanln(&s)
 }
 
 /* Channels are the pipes that connect concurrent goroutines.
  * You can send values into channels from one goroutine and receive those
  * values into another goroutine.
+ *
+ * Channel Direction:
+ *      Specify a direction on a channel type thus restricting to it either
+*       sending or receiving.
  */
