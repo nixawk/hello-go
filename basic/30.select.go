48a7585f6235416b60978b0fe77a906629a9cbd3
nixawk <hap.ddup@gmail.com>
2019-03-02 15:27:56 +0800

    select for channel

--- basic/30.select.go
+++ basic/30.select.go
@@ -8,21 +8,21 @@ func main() {
 	c2 := make(chan string)
 
 	go func() {
-		time.Sleep(1 * time.Second)
 		c1 <- "one"
 	}()
 
 	go func() {
-		time.Sleep(2 * time.Second)
 		c2 <- "two"
 	}()
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 5; i++ {
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
 		case msg2 := <-c2:
 			fmt.Println("received", msg2)
+		case <-time.After(time.Second):
+			fmt.Println("timeout")
 		}
 	}
 }
