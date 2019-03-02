8ca331bc7d8fe8a8c93342152faf40187f1af80c
nixawk <hap.ddup@gmail.com>
2019-03-02 08:46:07 +0800

    add map

--- basic/21.maps.go
+++ basic/21.maps.go
@@ -2,18 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-        var people map[string]string
-
-        people = make(map[string]string)
-
-        people["name"] = "Matz Giea"
-        people["flag"] = "AAzz"
-        people["dog"] = "Tedon"
-
-        for k := range people {
-                fmt.Printf("%s - %s\n", k, people[k])
+func pmap(m map[string]string) {
+        for k, v := range m {
+                fmt.Printf("%s = %s\n", k, v)
         }
+        fmt.Println("--------------------------------------")
+}
+
+func main() {
+
+        /* panic: assignment to entry in nil map */
+        /* maps have to be intialized before they can be used */
+        map1 := make(map[string]string)
+
+        map1["k1"] = "v1"
+        map1["k2"] = "v2"
+        map1["k3"] = "v3"
+
+        map2 := map[string]string{
+                "K1": "V1",
+                "K2": "V2",
+                "K3": "V3"}
+
+        /* delete */
+        pmap(map1)
+        delete(map1, "k3")
+        pmap(map1)
+
+        pmap(map2)
+        delete(map2, "K1")
+        pmap(map2)
+
+        fmt.Println(map1["k3"] == "")   /* true */
+        fmt.Println(map2["K1"] == "V1") /* false */
 }
 
 /* var map_var[key_data_type]value_data_type */
