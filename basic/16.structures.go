bc21e2e693c02794a412da840c398e04c70b6eac
nixawk <hap.ddup@gmail.com>
2019-03-02 13:39:39 +0800

    add struct

--- basic/16.structures.go
+++ basic/16.structures.go
@@ -4,22 +4,23 @@ import "fmt"
 
 type Person struct {
         name string
-        age int
+        age  int
+}
+
+func (p *Person) get_info() {
+        fmt.Printf("name: %s, age: %d\n", p.name, p.age)
 }
 
 func main() {
-        var kee Person                             /* struct without init */
-        var bob = Person{name: "Bob Ti", age: 25}  /* struct initialization */
-        var someone *Person                        /* struct pointer */
+        var kee Person                         /* struct without init */
+        bob := Person{name: "Bob Ti", age: 25} /* struct initialization */
 
         kee.name = "Kee Mitz"
         kee.age = 25
 
-        someone = &kee
+        kee.get_info()
+        bob.get_info()
 
-        fmt.Printf("name: %s, age: %d\n", kee.name, kee.age)
-        fmt.Printf("name: %s, age: %d\n", bob.name, bob.age)
-        fmt.Printf("name: %s, age: %d\n", someone.name, someone.age)
 }
 
 /* https://gobyexample.com/structs */
