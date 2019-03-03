aaf8a86255f0ed2129f067ffc567e73bb883dd68
Nixawk <hap.ddup@gmail.com>
2019-03-03 23:15:57 +0800

    init() and main()

--- basic/00.hello.go
+++ basic/00.hello.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/* All init function in any code that are part of the program will get called
+   before the main function */
+func init() {
+	fmt.Println("I'm the init func.")
+}
+
 func main() {
 	fmt.Println("Hello, World !")
 }
@@ -9,24 +15,15 @@ func main() {
 /* go run hello.go */
 /* go build hello.go && ./hello */
 
+/* $ go run 00.hello.go
+   I'm the init func.
+   Hello, World !
+*/
+
 /*
  * The first line of the program package main defines the package name in which
  * this program should lie. It is a mandatory statement, as Go programs run in
  * packages. The main package is the starting point to run the program. Each
  * package has a path and name associated with it.
 
-    debug@ulab:~/.golang$ ll go/src/fmt/
-    total 204
-    -rw-r--r-- 1 debug debug 14559 12月 15 07:36 doc.go
-    -rw-r--r-- 1 debug debug   551 12月 15 07:36 example_test.go
-    -rw-r--r-- 1 debug debug   219 12月 15 07:36 export_test.go
-    -rw-r--r-- 1 debug debug 56275 12月 15 07:36 fmt_test.go
-    -rw-r--r-- 1 debug debug 12673 12月 15 07:36 format.go
-    -rw-r--r-- 1 debug debug 30006 12月 15 07:36 print.go
-    -rw-r--r-- 1 debug debug 31951 12月 15 07:36 scan.go
-    -rw-r--r-- 1 debug debug 38408 12月 15 07:36 scan_test.go
-    -rw-r--r-- 1 debug debug  2156 12月 15 07:36 stringer_test.go
-
-*/
-
-/* https://gobyexample.com/hello-world */
+ */
