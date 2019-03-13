eee08a99a59a5f122567b79ac5770e763422a9c2
nixawk <hap.ddup@gmail.com>
2019-03-13 09:59:32 +0800

    arrays - create/index

--- basic/18.arrays.go
+++ basic/18.arrays.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	var a = []int{0, 1, 2, 3, 4}
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("i = %d\n", a[i])
+// Creating a new array looks like var array = [size]Type
+func create_array() {
+	a := [3]int{0, 1, 2}      // classical way
+	b := [...]int{0, 1, 2, 3} // a less verbose way
+	c := []int{0, 1, 2, 3, 4}
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+// Array values should be accessed using a number specifying the location of
+// the desired value in the array.
+// Indexes start at 0 and finish at array length -1.
+func index_array() {
+	type Person struct {
+		name string
+		age  int
+	}
+
+	ps := [2]Person{{name: "k", age: 20}, {name: "v", age: 25}}
+	for _, p := range ps {
+		fmt.Printf("name: %s, age: %d\n", p.name, p.age)
 	}
 }
 
-/*
- * var variable_name [SIZE] variable_type
- *
- * Go programming language provides a data structure called the array,
- * which can store a fixed-size sequential collection of elements of the
- * same type.
- */
+func main() {
+	create_array()
+	index_array()
+}
+
+/* https://www.programming-books.io/essential/go/f9ebde1b6f7b4ac49a7a4693f0f19a6a-array-indexes */
